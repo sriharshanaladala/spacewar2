dae6a5f785dcbf31fc5bf6893234e41b6252393e
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-23 14:00:29 +0530

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("space war")
+background = pygame.image.load("space for war.jpg")
 surface = pygame.image.load("ufo.png")
 pygame.display.set_icon(surface)
 
@@ -16,12 +17,14 @@ playerX_changed = 0
 enemyimg = pygame.image.load("alien (3).png")
 enemyX = random.randint(0, 740)
 enemyY = random.randint(0, 480)
-enemyX_changed = 0
+enemyX_changed = 0.3
+enemyY_changed = 40
 
 enemy1img = pygame.image.load("monster.png")
 enemy1X = random.randint(0, 740)
 enemy1Y = random.randint(0, 480)
-enemy1X_changed = 0
+enemy1X_changed = 0.3
+enemy1Y_changed = 70
 
 
 def player(x, y):
@@ -39,6 +42,7 @@ def enemy1(x, y):
 running = True
 while running:
     screen.fill((0, 0, 0))
+    screen.blit(background, (-3220, -2600))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,11 +58,28 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_changed = 0
                 # print("key stroke is released")
+# player boundries to stay with in screen
     playerX += playerX_changed
     if playerX <= 0:
         playerX = 0
     elif playerX >= 740:
         playerX = 740
+# enemy move ment with in screen
+    enemyX += enemyX_changed
+    if enemyX <= 0:
+        enemyX_changed = 0.3
+        enemyY += enemyY_changed
+    elif enemyX >= 740:
+        enemyX_changed = -0.3
+        enemyY += enemyY_changed
+    enemy1X += enemy1X_changed
+    if enemy1X <= 0:
+        enemy1X_changed = 0.3
+        enemy1Y += enemy1Y_changed
+    elif enemy1X >= 740:
+        enemy1X_changed = -0.3
+        enemy1Y += enemy1Y_changed
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     enemy1(enemy1X, enemy1Y)
